da2c28dc270667746ccbe1c1ef0fdf782bd8ca69
msf <marcossfelisberto@gmail.com>
2025-08-31 11:29:50 -0300

    Initial commit

---
+++ server/src/main/java/br/com/msp/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.msp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
