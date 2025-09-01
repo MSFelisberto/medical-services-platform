f045d9c5e2223a03ba81ef7332fc16c0cdc2345a
msf <marcossfelisberto@gmail.com>
2025-09-01 20:48:42 -0300

    Ajustes de configuração no api gateway

--- gateway/src/main/java/br/com/msp/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/br/com/msp/gateway/security/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Desabilita CSRF
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/**").permitAll()
                 )
